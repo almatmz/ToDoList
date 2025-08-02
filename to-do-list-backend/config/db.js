3fe475e0877e0f5741cd8d8c3b70bb2a1ba650a4
almat <almmuzdubai@gmail.com>
2025-08-02 20:09:46 +0500

    database connection fixing

--- to-do-list-backend/config/db.js
+++ to-do-list-backend/config/db.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // Force IPv4 connection
+  host: process.env.DB_HOST || undefined, // Let it use the host from connection string
+  family: 4, // Force IPv4
 });
 
 module.exports = pool;
